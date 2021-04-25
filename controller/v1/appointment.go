77553dc7f5058691ae33b7efd3a4185b3750e7a9
Your Name <you@example.com>
2021-04-25 17:40:40 +0800

    appointment

--- controller/v1/appointment.go
+++ controller/v1/appointment.go
@@ -2,6 +2,9 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/jinzhu/gorm"
+	"github.com/ucanme/fastgo/consts"
 	"github.com/ucanme/fastgo/controller/response"
 	"github.com/ucanme/fastgo/library/db"
 	"github.com/ucanme/fastgo/models"
@@ -10,6 +13,10 @@ import (
 )
 
 
+
+type AvailableDaysListResp struct {
+	Days []string `json:"days"`
+}
 func AvailableDaysList(c *gin.Context)  {
 	yesterDay := time.Now().Add(-time.Hour*24).Format("2006-01-02")
 	all := []models.Appointment{}
@@ -35,5 +42,57 @@ func AvailableDaysList(c *gin.Context)  {
 	if len(avaliDates) > 10 {
 		avaliDates = avaliDates[:10]
 	}
-	response.Success(c,avaliDates)
+	response.Success(c,AvailableDaysListResp{Days: avaliDates})
+}
+
+
+type AvailableHourListReq struct {
+	Day string `json:"day"`
+}
+
+type AvailableHourListResp []*AvailableHourInfo
+
+type AvailableHourInfo struct {
+	Hour int `json:"hour"`
+	Status int `json:"status"`
+}
+
+func AvaliableHoursList(c *gin.Context)  {
+	input := AvailableHourListReq{}
+	if err := c.ShouldBindWith(&input, binding.JSON); err != nil {
+		response.Fail(c, consts.PARAM_ERR_CODE, consts.PARAM_ERR.Error())
+		return
+	}
+	appoints := []models.Appointment{}
+	err := db.DB().Where("date=?",input.Day).Find(&appoints).Error
+	if err!=nil && err != gorm.ErrRecordNotFound{
+		response.Fail(c, consts.PARAM_ERR_CODE, consts.PARAM_ERR.Error())
+		return
+	}
+
+
+
+	hourMap := map[int]*AvailableHourInfo{}
+	avaliHours := []int{}
+	for _,v := range appoints{
+		if _,ok := hourMap[v.Hour];!ok{
+			hourMap[v.Hour] = &AvailableHourInfo{
+				Hour:   v.Hour,
+				Status: 1,
+			}
+			avaliHours = append(avaliHours,v.Hour)
+		}
+		if v.Status == 0 {
+			hourMap[v.Hour].Status = 0
+		}
+	}
+
+	sort.Slice(avaliHours, func(i, j int) bool {
+		return avaliHours[i] < avaliHours[j]
+	})
+	availableHourListResp := AvailableHourListResp{}
+	for _,v := range avaliHours{
+		availableHourListResp =append(availableHourListResp,hourMap[v])
+	}
+	response.Success(c,availableHourListResp)
 }
